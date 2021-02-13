1685fcdd25cd5ba2c49cfd6c973a46890e90c80c
Kanishka <123kanishka@gmail.com>
2021-02-13 10:18:55 +0530

    CI CD added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello React World2</p>
+        <p>Hello React World With CI/CD</p>
       </header>
     </div>
   );
